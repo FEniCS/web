43462c462fb54b0d91e6320d7a3b584f0a9d4078
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-02-01 13:23:50 +0000

    bcs

--- _test/test_code.py
+++ _test/test_code.py
@@ -18,7 +18,8 @@ imports = [
     "from ufl import inner, grad, dx, div, dot\n"
     "\n"
     "mesh = dolfinx.mesh.create_unit_cube(MPI.COMM_WORLD, 2, 3, 4)\n"
-    "f = ufl.grad(ufl.SpatialCoordinate(mesh)[1])",
+    "f = ufl.grad(ufl.SpatialCoordinate(mesh)[1])\n"
+    "bcs = []",
     "import gmsh"
 ]
 
