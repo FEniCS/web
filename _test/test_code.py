387c96abc6bbc6a5c35418001e6229a3d410e8b9
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-02-01 11:52:07 +0000

    run code

--- _test/test_code.py
+++ _test/test_code.py
@@ -1,10 +1,17 @@
 """Test that the code on the front page runs."""
 import os
+import pytest
 
 root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 
 
-def test_index_code():
+def test_index_code(has_fenicsx):
+    if not has_fenicsx:
+        try:
+            import dolfinx
+        except ImportError:
+            pytest.skip("DOLFINx must be installed to run this test.")
+
     with open(os.path.join(root_dir, "index.md")) as f:
         content = f.read()
     code = (
@@ -12,6 +19,6 @@ def test_index_code():
         "import dolfinx\n\n"
     )
     for i in content.split("```python")[1:]:
-        pass  # code += i.split("```")[0] + "\n\n"
+        code += i.split("```")[0] + "\n\n"
 
     exec(code)
