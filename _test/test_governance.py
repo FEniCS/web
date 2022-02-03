94e9033c02f88fe59027b51ca94d1c66d9525fe1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-02-03 11:36:52 +0000

    Add list of steering council and advisory board members to the website (#76)
    * Add steering council list
    
    * Add advisory board members

--- _test/test_governance.py
+++ _test/test_governance.py
@@ -26,3 +26,49 @@ def test_governance_institutions():
         for i in content.split("- [")[1:])
 
     assert partners == partners2
+
+
+def test_steering_council():
+    """Test that the steering council list agrees with governance repo."""
+    with open(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "../governance/steering-council.md"
+    )) as f:
+        content = f.read()
+    members = set(
+        i.split('"')[0]
+        for i in content.split('name="')[1:])
+
+    git = github.Github()
+
+    gov = git.get_repo("fenics/governance")
+    content = gov.get_contents("people.md").decoded_content.decode("utf8")
+    content = content.split("## Steering Council")[1].split("##")[0]
+    members2 = set(
+        " (".join(i.split(" (")[:-1])
+        for i in content.split("- ")[1:])
+
+    assert members == members2
+
+
+def test_advisory_board():
+    """Test that the advisory board list agrees with governance repo."""
+    with open(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "../governance/advisory-board.md"
+    )) as f:
+        content = f.read()
+    members = set(
+        i.split('"')[0]
+        for i in content.split('name="')[1:])
+
+    git = github.Github()
+
+    gov = git.get_repo("fenics/governance")
+    content = gov.get_contents("people.md").decoded_content.decode("utf8")
+    content = content.split("## Advisory board")[1].split("##")[0]
+    members2 = set(
+        " (".join(i.split(" (")[:-1])
+        for i in content.split("- ")[1:])
+
+    assert members == members2
--- governance/steering-council.md
+++ governance/steering-council.md
@@ -3,4 +3,52 @@ title: FEniCS Steering Council
 permalink: /steering-council/
 ---
 
-The current list of members of the FEniCS steering council can be found [here](https://github.com/FEniCS/governance/blob/master/people.md).
+The FEniCS steering council oversees the [governance](index.md) of the FEniCS community
+and makes decisions about the overall scope, vision and directions of the FEniCS project.
+
+## Steering council members
+
+{% include _developer.html
+  name="Michal Habera"
+  institution="University of Luxembourg"
+  github="michalhabera"
+  website="https://www2.karlin.mff.cuni.cz/~habera/"
+%}
+
+{% include _developer.html
+  name="Jack S. Hale"
+  institution="University of Luxembourg"
+  github="jhale"
+  website="https://jackhale.co.uk"
+%}
+
+{% include _developer.html
+  name="Chris Richardson"
+  institution="University of Cambridge"
+  github="chrisrichardson"
+%}
+
+{% include _developer.html
+  name="Johannes Ring"
+  institution="Simula Research Laboratory"
+  github="johannesring"
+%}
+
+{% include _developer.html
+  name="Marie E. Rognes"
+  institution="Simula Research Laboratory"
+  github="meg-simula"
+%}
+
+{% include _developer.html
+  name="Nathan (Nate) Sime"
+  institution="Carnegie Institution for Science"
+  github="nate-sime"
+%}
+
+{% include _developer.html
+  name="Garth N. Wells"
+  institution="University of Cambridge"
+  github="garth-wells"
+  website="http://www3.eng.cam.ac.uk/~gnw20/"
+%}
