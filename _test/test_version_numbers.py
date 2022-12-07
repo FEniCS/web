86b9c9b976a29cd5fe888604aed20e2dd271ac1b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-12-07 14:01:53 +0000

    update test to allow date from any matching version (#119)
    * update date
    
    * update test
    
    * flake
    
    * .name

--- _test/test_version_numbers.py
+++ _test/test_version_numbers.py
@@ -19,7 +19,6 @@ def test_fenicsx_version_number():
     dolfinx = git.get_repo("fenics/dolfinx")
 
     latest_tag = dolfinx.get_tags()[0]
-    commit = dolfinx.get_commit(latest_tag.commit.sha)
 
     name = latest_tag.name
     p = name.rfind('.')
@@ -27,10 +26,16 @@ def test_fenicsx_version_number():
     if name[0] == "v":
         name = name[1:]
 
-    year, month, _ = commit.raw_data["commit"]["author"]["date"].split("-", 2)
-    year = int(year)
-    month = int(month)
-    date = datetime.datetime(year=year, month=month, day=1)
+    acceptable_dates = []
+    for tag in dolfinx.get_tags():
+        if tag.name.startswith(name) or tag.name.startswith("v" + name):
+            c = dolfinx.get_commit(tag.commit.sha)
+
+            y, m, _ = c.raw_data["commit"]["author"]["date"].split("-", 2)
+            y = int(y)
+            m = int(m)
+            date = datetime.datetime(year=y, month=m, day=1)
+            acceptable_dates.append(date.strftime("%B %Y"))
 
     assert str(config["fenicsxversion"]) == name
-    assert config["fenicsxversiondate"] == date.strftime("%B %Y")
+    assert config["fenicsxversiondate"] in acceptable_dates
